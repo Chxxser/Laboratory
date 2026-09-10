b073d5b41996c8c4c5dee3d77070db3d3dd4439e
Artem <artemdemidenko2@gmail.com>
2026-09-10 09:11:31 +0700

    Оформил класс Phone (характеристики телефона)

--- Model/Phone.cs
+++ Model/Phone.cs
@@ -8,5 +8,20 @@ namespace Model
 {
     public class Phone
     {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Color { get; set; }
+        public int Memory { get; set; }
+        public decimal Price { get; set; }
+        public bool Availability { get; set; }
+        public override string ToString()
+        {
+            string status;
+            if (Availability == true) { status = "В наличии"; }
+            else { status = "Продан"; }
+            return ($"{Id}. {Brand} {Model} ({Year}) - {Color}, {Memory} ГБ, {Price} руб. - {status}");
+        }
     }
 }
